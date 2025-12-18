eefe4cd67b75ba56ad2f33356955b3a036742667
Gripen <jas39danm@gmail.com>
2025-12-18 22:37:23 +0800

    Refactor error handling in RepoError enum: rename Disconnect to IO and Redaction to Database for clarity

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,3 +1,4 @@
+//use crate::repo::TableName;
 use std::io;
 use std::path::PathBuf;
 use thiserror::Error;
@@ -7,7 +8,7 @@ pub enum RepoError {
     #[error("Repository does not exist at the specified path and it's parent directories: {0}")]
     RepoNotFound(PathBuf),
     #[error("i/o Error: {0}")]
-    Disconnect(#[from] io::Error),
+    IO(#[from] io::Error),
     #[error("database Error: {0}")]
-    Redaction(#[from] rusqlite::Error),
+    Database(#[from] rusqlite::Error),
 }
